2cee3d37ef68dcf5e0eb6c5cff04e13987ede1e0
Daud Khan <daudu6944@gmail.com>
2025-04-26 19:44:36 +0500

    Add: signin, singup functionality & protected route

--- app.js
+++ app.js
@@ -1,44 +1,78 @@
+const path = require("path");
+const bcrypt = require("bcrypt");
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { userModel, postModel } = require("./models");
 
 const server = express();
+server.set("view engine", "ejs");
+
+server.use(cookieParser());
+server.use(express.json());
+server.use(express.urlencoded());
+server.use(express.static(path.join(__dirname, "public")));
 
 server.get("/", (req, res) => {
-  res.send("Listening");
+  res.render("signup");
 });
 
-server.get("/create", async (req, res) => {
-  const createdUser = await userModel.create({
-    username: "daudk4",
-    email: "daudu6944@gmail.com",
-    age: "24",
+server.get("/signin", (req, res) => {
+  res.render("signin");
+});
+
+server.get("/dashboard", isLoggedIn, (req, res) => {
+  res.send(req.user);
+});
+
+server.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) return res.status(500).send("Invalid Credentials");
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) return res.status(500).send("Invalid Credentials");
+
+//   res.status(200).send("you can login");
+  const token = jwt.sign({ email, userId: user._id }, "secretKey");
+    res.cookie("token", token);
+    res.redirect('/dashboard')
+
+});
+
+server.post("/signup", async (req, res) => {
+  const { username, name, email, age, password } = req.body;
+  const user = await userModel.findOne({ email });
+
+  if (user) return res.status(500).send("User already registered");
+
+  bcrypt.genSalt(12, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      const createdUser = await userModel.create({
+        username,
+        name,
+        email,
+        age,
+        password: hash,
+      });
+
+      const token = jwt.sign({ email, userId: createdUser._id }, "secretKey");
+      res.cookie("token", token);
+      res.redirect("/signin");
+    });
   });
-
-  res.send(createdUser);
 });
 
-server.get("/post/create", async (req, res) => {
-  const post = await postModel.create({
-    postdata: "Creating my first post in db",
-    user: "680c8b65d41e1f346ea01cab",
-  });
-
-  const user = await userModel.findOneAndUpdate(
-    { _id: "680c8b65d41e1f346ea01cab" },
-    { $push: { posts: post._id } },
-    { new: true }
-  );
-
-  res.send({ post, user });
-});
-
-server.get("/delete", async (req, res) => {
-  const deletedUser = await userModel.findOneAndDelete({
-    email: "daudu6944@gmail.com",
-  });
-
-  res.send(deletedUser);
-});
+//middleware function:
+function isLoggedIn(req, res, next) {
+  if (!req.cookies.token) res.send("You must be logged in");
+  else {
+    const data = jwt.verify(req.cookies.token, "secretKey");
+    req.user = data;
+  }
+  next();
+}
 
 server.listen(3000, () => {
   console.log("Server is up and ready...");
